932b4468f297202790a70d2c0c04f7b36b8217dd
cryphon <yvanroes@gmail.com>
2025-02-09 03:13:45 +0100

    fix: moved css to separate file/folder for practical use

--- src/IMVI.py
+++ src/IMVI.py
@@ -4,8 +4,8 @@ import sys
 import cv2
 from PyQt5.QtCore import QMimeData, QRect, Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget
-from interface import Interface
 
+from interface import Interface
 from list_widget import ListWidget
 
 
@@ -25,45 +25,12 @@ class MainWindow(QMainWindow):
         conversion_tab = Interface()
         tab_widget.addTab(conversion_tab, "Convert Images to Video")
 
-        self.setStyleSheet("""
-            QMainWindow { 
-    background-color: #1e1e1e; 
-}
-QLabel { 
-    color: #e0e0e0; 
-    font-size: 14px; 
-}
-QPushButton {
-    background-color: #2d5e7e;
-    color: #ffffff;
-    padding: 6px;
-    border: none;
-    border-radius: 4px;
-}
-QPushButton:hover { 
-    background-color: #3d7eae; 
-}
-QTabWidget::pane {
-    border: 1px solid #333333;
-    border-radius: 8px;
-    margin-top: 20px;
-}
-QTabBar::tab {
-    background: #2d2d2d;
-    padding: 10px;
-    border-top-left-radius: 8px;
-    border-top-right-radius: 8px;
-}
-QTabBar::tab:selected {
-    background: #3d3d3d;
-    color: #ffffff;
-    border-bottom: 2px solid #2d5e7e;
-}
-        """)
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    qss = "css/style.qss"
+    with open(qss, "r") as fh:
+        app.setStyleSheet(fh.read())
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
