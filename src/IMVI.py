6021efebbf3827644269dd7644ff0ef2c44017f6
cryphon <yvanroes@gmail.com>
2025-02-13 00:25:00 +0100

    fix: removed some comments

--- src/IMVI.py
+++ src/IMVI.py
@@ -18,24 +18,19 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(400, 600)
         self.setWindowFlag(Qt.FramelessWindowHint)
 
-        # Create a container widget to hold both header and content
         self.container = QWidget()
         self.setCentralWidget(self.container)
 
-        # Create a vertical layout for the container
         self.layout = QVBoxLayout(self.container)
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.layout.setSpacing(0)
 
-        # Add the custom header
         self.header = CustomHeader(self)
         self.layout.addWidget(self.header)
 
-        # Create and add the tab widget
         self.tab_widget = QTabWidget()
         self.layout.addWidget(self.tab_widget)
 
-        # Add your conversion tab
         conversion_tab = Interface()
         self.tab_widget.addTab(conversion_tab, "Convert Images to Video")
 
